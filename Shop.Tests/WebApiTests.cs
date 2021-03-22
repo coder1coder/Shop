5cb27755597e92fd3be3905efdeab8f9b3aa0362
Михаил Кодер <coder1coder@outlook.com>
2021-03-22 19:21:32 +0300

    add Tests project

---
+++ Shop.Tests/WebApiTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using System.Net.Http;
+
+namespace Shop.Tests
+{
+    public class WebApiTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ProductCreate_ShouldReturnProduct()
+        {
+            Assert.Pass();
+
+            
+        }
+    }
+}
